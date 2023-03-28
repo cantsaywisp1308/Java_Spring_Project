45da1fa70be80a842e5be6c2fd060c09d668cc5a
cantsaywisp1308 <73329048+cantsaywisp1308@users.noreply.github.com>
2023-03-27 18:58:10 -0700

    upload main source code

---
+++ src/main/java/com/coeding/repository/TypeRepository.java
@@ -0,0 +1,15 @@
+package com.coeding.repository;
+
+import com.coeding.entity.Brand;
+import com.coeding.entity.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface TypeRepository extends JpaRepository<Type,Long>  {
+    List<Type> findByCategoryId(Long id);
+}
+//commit
